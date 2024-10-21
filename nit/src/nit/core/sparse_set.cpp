6241c96857c1da4857e7acd52a150dc2d9a16762
Alex <asu4net@gmail.com>
2024-10-21 20:05:30 +0200

    Improved sparse set

--- nit/src/nit/core/sparse_set.cpp
+++ nit/src/nit/core/sparse_set.cpp
@@ -32,14 +32,12 @@ namespace Nit
         sparse_set->sparse = new u32[max];
         sparse_set->dense  = new u32[max];
         memset(sparse_set->sparse, SparseSet::INVALID_INDEX, sizeof(u32) * max);
-        
-        // Not needed, just for debugging clarity
-        memset(sparse_set->dense,  SparseSet::INVALID_INDEX, sizeof(u32) * max);
     }
     
     void Insert(SparseSet* sparse_set, u32 element)
     {
         NIT_CHECK(IsValid(sparse_set) && !IsFull(sparse_set));
+        NIT_CHECK(!Test(sparse_set, element));
         
         u32 next_slot = sparse_set->count;
         sparse_set->sparse[element] = next_slot;
@@ -47,22 +45,35 @@ namespace Nit
         ++sparse_set->count;
     }
 
+    bool Test(SparseSet* sparse_set, u32 element)
+    {
+        NIT_CHECK(IsValid(sparse_set));
+        return element < sparse_set->max && sparse_set->sparse[element] != SparseSet::INVALID_INDEX;
+    }
+
     u32 Search(SparseSet* sparse_set, u32 element)
     {
         NIT_CHECK(IsValid(sparse_set));
-        
-        if (sparse_set->sparse[element] != SparseSet::INVALID_INDEX && sparse_set->dense[sparse_set->sparse[element]])
+
+        if (element >= sparse_set->max)
         {
-            return sparse_set->sparse[element];
+            return SparseSet::INVALID_INDEX;
+        }
+
+        u32 dense_index = sparse_set->sparse[element];
+        
+        if (dense_index == SparseSet::INVALID_INDEX)
+        {
+            return SparseSet::INVALID_INDEX;
         }
         
-        return SparseSet::INVALID_INDEX;
+        return dense_index;
     }
     
     void Delete(SparseSet* sparse_set, u32 element)
     {
-        NIT_CHECK(IsValid(sparse_set));
-        NIT_CHECK(Search(sparse_set, element) != SparseSet::INVALID_INDEX);
+        NIT_CHECK(IsValid(sparse_set) && sparse_set->count > 0);
+        NIT_CHECK(Test(sparse_set, element));
         
         u32 deleted_slot = sparse_set->sparse[element];
         u32 last_slot = sparse_set->count - 1;
@@ -72,17 +83,44 @@ namespace Nit
 
         u32 last_element = sparse_set->dense[last_slot];
         sparse_set->dense[deleted_slot] = last_element;
-        sparse_set->sparse[last_element] = sparse_set->dense[deleted_slot];
+        sparse_set->sparse[last_element] = deleted_slot;
+    }
 
-        // Not needed, just for debugging clarity
-        sparse_set->dense[last_slot] = SparseSet::INVALID_INDEX;
+    void Resize(SparseSet* sparse_set, u32 new_max)
+    {
+        NIT_CHECK(IsValid(sparse_set) && new_max > sparse_set->max);
+        
+        u32* new_sparse = new u32[new_max];
+        u32* new_dense  = new u32[new_max];
+    
+        std::copy_n(sparse_set->sparse, sparse_set->max, new_sparse);
+        memset(new_sparse + sparse_set->max, SparseSet::INVALID_INDEX, (new_max - sparse_set->max) * sizeof(u32));
+        
+        std::copy_n(sparse_set->dense, sparse_set->max, new_dense);
+    
+        delete[] sparse_set->sparse;
+        delete[] sparse_set->dense;
+    
+        sparse_set->sparse = new_sparse;
+        sparse_set->dense  = new_dense;
+        sparse_set->max    = new_max;
     }
 
     void Free(SparseSet* sparse_set)
     {
         NIT_CHECK(IsValid(sparse_set));
-        delete [] sparse_set->sparse;
-        delete [] sparse_set->dense;
-        *sparse_set = {};
+        
+        if (sparse_set->sparse)
+        {
+            delete[] sparse_set->sparse;
+            sparse_set->sparse = nullptr;
+        }
+        if (sparse_set->dense)
+        {
+            delete[] sparse_set->dense;
+            sparse_set->dense = nullptr;
+        }
+        
+        sparse_set->count = sparse_set->max = 0;
     }
 }
--- nit/src/nit/core/sparse_set.h
+++ nit/src/nit/core/sparse_set.h
@@ -12,14 +12,15 @@ namespace Nit
         u32  max    = 0;
     };
     
-    //TODO: Resize
     bool IsLoaded(SparseSet* sparse_set);
     bool IsValid(SparseSet* sparse_set);
     bool IsEmpty(SparseSet* sparse_set);
     bool IsFull(SparseSet* sparse_set);
     void Load(SparseSet* sparse_set, u32 max);
     void Insert(SparseSet* sparse_set, u32 element);
+    bool Test(SparseSet* sparse_set, u32 element);
     u32  Search(SparseSet* sparse_set, u32 element);
     void Delete(SparseSet* sparse_set, u32 element);
+    void Resize(SparseSet* sparse_set, u32 new_max);
     void Free(SparseSet* sparse_set);
 }
