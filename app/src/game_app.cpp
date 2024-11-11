887e174bc3c00eff4afdedbb7639a37e6fafffa9
Alex <asu4net@gmail.com>
2024-11-11 21:57:59 +0100

    Update game_app.cpp

--- app/src/game_app.cpp
+++ app/src/game_app.cpp
@@ -3,15 +3,15 @@
 
 using namespace nit;
 
-void OnRun();
-void GameStart();
-void GameUpdate();
+void on_run();
+void game_start();
+void game_update();
 
 int main(int argc, char** argv)
 {
     Engine engine_instance;
     engine::set_instance(&engine_instance);
-    engine::run(OnRun);
+    engine::run(on_run);
 }
 
 // -----------------------------------------------------------------
@@ -34,7 +34,7 @@ AssetHandle test_texture;
 
 // -----------------------------------------------------------------
 
-void ResetMovement(Transform& transform, Move& move)
+void reset_movement(Transform& transform, Move& move)
 {
     float speed      = GetRandomValue(MIN_SPEED, MAX_SPEED);
     move.destination = RandomPointInSquare(RECT_LEFT.x, RECT_RIGHT.y, RECT_RIGHT.x, RECT_LEFT.y);
@@ -43,7 +43,7 @@ void ResetMovement(Transform& transform, Move& move)
 
 // -----------------------------------------------------------------
 
-void SpawnEntity()
+void spawn_entity()
 {
     Entity entity = CreateEntity();
     Vector3 position = ToVector3(RandomPointInSquare(RECT_LEFT.x, RECT_RIGHT.y, RECT_RIGHT.x, RECT_LEFT.y)); 
@@ -52,17 +52,17 @@ void SpawnEntity()
     AddComponent<Sprite>(entity).tint = GetRandomColor();
     //GetComponent<Sprite>(entity).texture = test_texture;
     SetSpriteSubTexture2D(GetComponent<Sprite>(entity), "cpp");
-    ResetMovement(GetComponent<Transform>(entity), AddComponent<Move>(entity));
+    reset_movement(GetComponent<Transform>(entity), AddComponent<Move>(entity));
 }
 
 // -----------------------------------------------------------------
 
-void OnRun()
+void on_run()
 {
     //Create game system
     CreateSystem("Game", 1);
-    SetSystemCallback(GameStart,   Stage::Start);
-    SetSystemCallback(GameUpdate,  Stage::Update);
+    SetSystemCallback(game_start,   Stage::Start);
+    SetSystemCallback(game_update,  Stage::Update);
 
     RegisterComponentType<Move>();
     CreateEntityGroup<Transform, Sprite, Move>();
@@ -70,7 +70,7 @@ void OnRun()
 
 // -----------------------------------------------------------------
 
-void GameStart()
+void game_start()
 {
     AssetHandle test_scene = find_asset_by_name("test_scene");
 
@@ -82,7 +82,7 @@ void GameStart()
     test_texture = find_asset_by_name("test_sheet");
 }
 
-void GameUpdate()
+void game_update()
 {
     //SpawnEntity();
     
@@ -93,7 +93,7 @@ void GameUpdate()
 
         if (Distance(ToVector2(transform.position), move.destination) < 0.1f)
         {
-            ResetMovement(transform, move);
+            reset_movement(transform, move);
         }
         else
         {
