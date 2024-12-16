67d42850f85f04d47dfcc33b772c17cc52eea080
Alex <asu4net@gmail.com>
2024-12-16 10:53:08 +0100

    Update main.cpp

--- app/src/main.cpp
+++ app/src/main.cpp
@@ -2,25 +2,16 @@
 
 using namespace nit;
 
-// -----------------------------------------------------------------
-
 constexpr Vector2 RECT_LEFT  = { -100.f, 100.f };
 constexpr Vector2 RECT_RIGHT = { 100.f, -100.f };
 constexpr float MIN_SPEED = 1.f;
 constexpr float MAX_SPEED = 15.f;
-
-// -----------------------------------------------------------------
-
 
 struct Move
 {
     Vector2 velocity;
     Vector2 destination;
 };
-
-AssetHandle test_texture;
-
-// -----------------------------------------------------------------
 
 void reset_movement(Transform& transform, Move& move)
 {
@@ -29,16 +20,12 @@ void reset_movement(Transform& transform, Move& move)
     move.velocity    = normalize(move.destination - to_v2(transform.position)) * speed;
 }
 
-// -----------------------------------------------------------------
-
 void spawn_entity()
 {
     EntityID entity = entity_create();
     Vector3 position = to_v3(random_point_in_square(RECT_LEFT.x, RECT_RIGHT.y, RECT_RIGHT.x, RECT_LEFT.y)); 
     entity_add<Transform>(entity).position = position;
-    //add<Transform>(entity).position = V3_ZERO;
     entity_add<Sprite>(entity).tint = GetRandomColor();
-    //get<Sprite>(entity).texture = test_texture;
     sprite_set_sub_texture(entity_get<Sprite>(entity), "cpp");
     reset_movement(entity_get<Transform>(entity), entity_add<Move>(entity));
 }
@@ -46,14 +33,6 @@ void spawn_entity()
 // -----------------------------------------------------------------
 
 void init();
-
-
-
-struct Box
-{
-    Vector3 position;
-    Vector3 scale;
-};
 
 int main(int argc, char** argv)
 {
@@ -75,48 +54,24 @@ void init()
     entity_create_group<Transform, Sprite, Move>();
 }
 
-// -----------------------------------------------------------------
-
 AssetHandle test_scene;
-EntityID    visitor;
-EntityID    trigger;
-
-ListenerAction trigger_enter(const TriggerEnterArgs& args)
-{
-    
-    return ListenerAction::StayListening;
-}
 
 ListenerAction start()
 {
-    test_scene = asset_find_by_name("physics scene");
+    test_scene = asset_find_by_name("test scene");
 
     if (asset_valid(test_scene))
     {
         asset_load(test_scene);
-        visitor = entity_find_by_name("visitor");
-        trigger = entity_find_by_name("trigger");
-        
-        entity_get<TriggerEvents>(trigger).enter_event += Listener<const TriggerEnterArgs&>::create(trigger_enter);
     }
-
-    test_texture = asset_find_by_name("test sheet");
-
+    
     return ListenerAction::StayListening;
 }
 
 ListenerAction update()
 {
-    static bool first_frame = true;
+    //spawn_entity();
     
-    if (first_frame)
-    {
-        rigidbody_add_force(entity_get<Rigidbody2D>(visitor), V2_LEFT * 30.f, to_v2(entity_get<Transform>(visitor).position));
-        first_frame = false;
-    }
-    
-    f32 speed = 3.f;
-
     for (EntityID entity : entity_get_group<Transform, Sprite, Move>().entities)
     {
         auto& transform = entity_get<Transform>(entity);
@@ -134,42 +89,3 @@ ListenerAction update()
 
     return ListenerAction::StayListening;
 }
-
-/*#include "nit.h"
-
-int main(int argc, char** argv)
-{
-    using namespace nit;
-
-    window_init({ .start_maximized = false });
-    audio_init();
-    renderer_2d_init();
-
-    Camera camera;
-    camera.projection = CameraProjection::Orthographic;
-    
-    Transform camera_transform;
-    camera_transform.position = { 0, 0, 3 };
-    
-    Vector3 quad_pos;
-    
-    while (!window_should_close())
-    {
-        clear_screen();
-        
-        quad_pos = quad_pos + V3_RIGHT * 0.0001f;
-
-        // position, rotation, scale
-
-        Matrix4 camera_matrix = camera_proj_view(camera, camera_transform);
-        
-        begin_scene_2d(camera_matrix);
-        {
-            draw_quad(quad_pos, V3_ZERO, V3_ONE, V4_COLOR_ORANGE);
-            draw_quad(V3_ZERO);
-        }
-        end_scene_2d();
-        
-        window_update();
-    }
-}*/
